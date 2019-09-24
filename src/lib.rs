a6dc9e7a84a27994aeb1e5f0fa816f5c9a5ec637
nanonicholson <nanonicholson@gmail.com>
2019-09-24 20:24:10 +0600

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,22 @@ mod lexer;
 mod parse;
 mod read_token;
 mod read_pattern;
-mod patterns;
+mod patterns {
+    mod pattern;
+    mod or_pattern;
+    mod and_pattern;
+    mod range_pattern;
+    mod many_pattern;
+
+    pub use pattern::*;
+    pub use or_pattern::*;
+    pub use and_pattern::*;
+    pub use range_pattern::*;
+    pub use many_pattern::*;
+}
+
+pub use lexer::*;
+pub use parse::*;
+pub use read_token::ReadToken;
+pub use read_pattern::ReadPattern;
+pub use patterns::pat;
--- src/patterns/range_pattern.rs
+++ src/patterns/range_pattern.rs
@@ -35,3 +35,47 @@ impl<T, R> ReadPattern for RangePattern<T, R> where
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::pat;
+    use crate::ReadPattern;
+
+    #[test]
+    fn range_pattern() {
+        let full = pat("a") * ..;
+        assert_eq!(full.read_pattern(""), Some(0));
+        assert_eq!(full.read_pattern("a"), Some(1));
+        assert_eq!(full.read_pattern("aa"), Some(2));
+        assert_eq!(full.read_pattern("aaa"), Some(3));
+
+        let from = pat("b") * (2..);
+        assert_eq!(from.read_pattern(""), None);
+        assert_eq!(from.read_pattern("b"), None);
+        assert_eq!(from.read_pattern("bb"), Some(2));
+        assert_eq!(from.read_pattern("bbb"), Some(3));
+
+        let to = pat("c") * ..2;
+        assert_eq!(to.read_pattern(""), Some(0));
+        assert_eq!(to.read_pattern("c"), Some(1));
+        assert_eq!(to.read_pattern("cc"), Some(1));
+
+        let to_inclusive = pat("d") * ..=2;
+        assert_eq!(to_inclusive.read_pattern(""), Some(0));
+        assert_eq!(to_inclusive.read_pattern("d"), Some(1));
+        assert_eq!(to_inclusive.read_pattern("dd"), Some(2));
+        assert_eq!(to_inclusive.read_pattern("ddd"), Some(2));
+
+        let range = pat("e") * (1..3);
+        assert_eq!(range.read_pattern(""), None);
+        assert_eq!(range.read_pattern("e"), Some(1));
+        assert_eq!(range.read_pattern("ee"), Some(2));
+        assert_eq!(range.read_pattern("eee"), Some(2));
+
+        let range_inclusive = pat("f") * (1..=2);
+        assert_eq!(range_inclusive.read_pattern(""), None);
+        assert_eq!(range_inclusive.read_pattern("f"), Some(1));
+        assert_eq!(range_inclusive.read_pattern("ff"), Some(2));
+        assert_eq!(range_inclusive.read_pattern("fff"), Some(2));
+    }
+}
