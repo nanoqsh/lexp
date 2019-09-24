098430e29b0d12a2f4fe16fe9352d1839be69fac
nanonicholson <nanonicholson@gmail.com>
2019-09-24 18:52:54 +0600

    Add Pattern tests

--- src/patterns/pattern.rs
+++ src/patterns/pattern.rs
@@ -109,3 +109,86 @@ impl<T> Mul<RangeInclusive<u32>> for Pattern<T> where
         Pattern(RangePattern(self.0, rhs))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pattern_str() {
+        let pattern = Pattern("some text");
+        assert_eq!(pattern.read_pattern("some text"), Some("some text".len()));
+        assert_eq!(pattern.read_pattern("some text_"), Some("some text".len()));
+        assert_eq!(pattern.read_pattern("text"), None);
+        assert_eq!(pattern.read_pattern(""), None);
+    }
+
+    #[test]
+    fn pattern_char() {
+        let pattern = Pattern('a');
+        assert_eq!(pattern.read_pattern("a"), Some(1));
+        assert_eq!(pattern.read_pattern("ab"), Some(1));
+        assert_eq!(pattern.read_pattern("b"), None);
+    }
+
+    #[test]
+    fn combine_patterns() {
+        let a = Pattern("a") | "b" | "c";
+        let b = Pattern("0") & "" & "1";
+
+        let ab = a & b;
+        assert_eq!(ab.read_pattern("a01"), Some(3));
+        assert_eq!(ab.read_pattern("b01"), Some(3));
+        assert_eq!(ab.read_pattern("c01"), Some(3));
+        assert_eq!(ab.read_pattern("c0"), None);
+
+        let a_b = a | b;
+        assert_eq!(a_b.read_pattern("a"), Some(1));
+        assert_eq!(a_b.read_pattern("b"), Some(1));
+        assert_eq!(a_b.read_pattern("c"), Some(1));
+        assert_eq!(a_b.read_pattern("01"), Some(2));
+        assert_eq!(a_b.read_pattern("0"), None);
+
+        let aaa = a * 3;
+        assert_eq!(aaa.read_pattern("aaa"), Some(3));
+        assert_eq!(aaa.read_pattern("abc"), Some(3));
+        assert_eq!(aaa.read_pattern("bac"), Some(3));
+
+        let bb = b * 2;
+        assert_eq!(bb.read_pattern("0101"), Some(4));
+        assert_eq!(bb.read_pattern("01"), None);
+        assert_eq!(bb.read_pattern("010101"), Some(4));
+
+        let a_aaa = a * (1..=3);
+        assert_eq!(a_aaa.read_pattern("b"), Some(1));
+        assert_eq!(a_aaa.read_pattern("cb"), Some(2));
+        assert_eq!(a_aaa.read_pattern("bca"), Some(3));
+        assert_eq!(a_aaa.read_pattern("aabb"), Some(3));
+        assert_eq!(a_aaa.read_pattern(""), None);
+
+        let b_bb = b * (1..3);
+        assert_eq!(b_bb.read_pattern("01"), Some(2));
+        assert_eq!(b_bb.read_pattern("0101"), Some(4));
+        assert_eq!(b_bb.read_pattern("010101"), Some(4));
+        assert_eq!(b_bb.read_pattern(""), None);
+
+        let not_a_3 = Pattern(|c: char| c != 'a') * 3;
+        assert_eq!(not_a_3.read_pattern("zzz"), Some(3));
+        assert_eq!(not_a_3.read_pattern("zz"), None);
+        assert_eq!(not_a_3.read_pattern("zzzz"), Some(3));
+        assert_eq!(not_a_3.read_pattern("aaa"), None);
+        assert_eq!(not_a_3.read_pattern("zza"), None);
+
+        let whitespace_or_alpha = Pattern(char::is_whitespace) | char::is_alphabetic;
+        assert_eq!(whitespace_or_alpha.read_pattern("a"), Some(1));
+        assert_eq!(whitespace_or_alpha.read_pattern(" "), Some(1));
+        assert_eq!(whitespace_or_alpha.read_pattern("*"), None);
+
+        let w_a_range = whitespace_or_alpha * (1..=2);
+        assert_eq!(w_a_range.read_pattern("a"), Some(1));
+        assert_eq!(w_a_range.read_pattern("a "), Some(2));
+        assert_eq!(w_a_range.read_pattern(""), None);
+        assert_eq!(w_a_range.read_pattern("z+"), Some(1));
+        assert_eq!(w_a_range.read_pattern(" f "), Some(2));
+    }
+}
